e3b0d6e2c7e5e999c36edb44e516e822f01dd771
Srikanth Muthuvel Ganthimathi <smuthuvelgan@binghamton.edu>
2025-12-22 03:02:08 -0500

    Refactor regfile by cleaning comments and logic
    Removed commented header and updated write condition.

--- regfile.sv
+++ regfile.sv
@@ -1,23 +1,5 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 11/12/2025 09:36:49 PM
-// Design Name: 
-// Module Name: regfile
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
+
 module regfile(
 input logic clk,
 input logic [4:0] ra1, ra2, wa,
@@ -32,7 +14,7 @@ output logic [31:0] rd1, rd2
     assign rd2 = (ra2 == 0)? 32'd0 : x[ra2];
     
     always@(posedge clk) begin
-    if((we && wa) != 0) begin 
+    if (we && (wa != 5'd0))begin
     x[wa] <= wd;
     end
     end
